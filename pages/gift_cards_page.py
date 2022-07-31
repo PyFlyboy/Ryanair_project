ca172d9efc3ba1cd3674d732cbc102b170800564
Tomasz Kraczka <kraczkatomasz@gmail.com>
2022-07-31 13:39:06 +0200

    New method added. 1. get_voucher_currency - return a dictonary where key is a currency code and value is their xpath locator 2. get_currency_value - return a dictornay where keys are currency code and values are list of available amounts (xpath locator)

--- pages/gift_cards_page.py
+++ pages/gift_cards_page.py
@@ -1,3 +1,4 @@
+import sys
 import time
 from utilities import custom_logger as cl
 import logging
@@ -35,10 +36,21 @@ class GiftCardsPage(BasePage):
             self.element_click(locator=voucher_location, locator_type="xpath")
             driver.implicitly_wait(2)
             self.screen_shot("Voucher")
-        driver.execute_script("window.scrollTo(0,400);")
-        self.wait_for_element(self.locator['currency_option'], locator_type="xpath")
-        print(self.get_element_list(self.locator['currency_option'], locator_type="xpath"))
-        print(self.get_element_list(self.locator['currency_amount_option'], locator_type="xpath"))
+        test = self.get_voucher_currency()
+        self.get_currency_value(test)
+
+
+        # self.wait_for_element(self.locator['currency_option'], locator_type="xpath")
+        # currency_list= self.get_element_list(self.locator['currency_option'], locator_type="xpath")
+        # curr_value_list = self.get_element_list(self.locator['currency_amount_option'], locator_type="xpath")
+        # new_list = []
+        # for item in currency_list:
+        #     xpath_item = "//select[starts-with(@id,'currencySelector')]/option[@value='{}']".format(item.get_attribute("value"))
+        #     new_list.append(xpath_item)
+        #
+        # self.element_click(self.locator['currency_selector'], locator_type="xpath")
+        # self.element_click(locator=new_list[5], locator_type="xpath")
+        # print(new_list, file=sys.stderr)
 
 
     def voucher_selection_check(self):
@@ -54,8 +66,45 @@ class GiftCardsPage(BasePage):
             driver.implicitly_wait(2)
             self.screen_shot("Voucher")
 
+    def get_voucher_currency(self):
+        driver.execute_script("window.scrollTo(0,400);")
+        self.wait_for_element(self.locator['currency_option'], locator_type="xpath")
+        currency = self.get_element_list(self.locator['currency_option'], locator_type="xpath")
+        currency_xpath_list = []
+        currency_value_list = []
+        for item in currency:
+            currency_xpath = "//select[starts-with(@id,'currencySelector')]/option[@value='{}']".format(item.get_attribute("value"))
+            currency_value = item.get_attribute("value")
+            currency_xpath_list.append(currency_xpath)
+            currency_value_list.append(currency_value)
+        currency_val = dict(zip(currency_value_list,currency_xpath_list))
+        return currency_val
 
+    def get_currency_value(self, currency_types=dict):
+            print(currency_types)
+            if currency_types:
+                currency_values = {}
 
+                for key in list(currency_types.keys()):
+                    print(key, file=sys.stderr)
+                    self.element_click(locator=currency_types[key], locator_type="xpath")
+                    currency_amount = self.get_element_list(self.locator['currency_amount_option'],
+                                                                locator_type="xpath")
+                    curr_ammount = []
+                    for element in currency_amount:
+                        element_xpath = "//select[starts-with(@id,'amountSelector')]/option[@value='{}']".format(
+                        element.get_attribute("value"))
+                        print(element_xpath)
+                        curr_ammount.append(element_xpath)
+
+                    currency_values[key] = curr_ammount
+
+            return currency_values
+
+        # self.element_click(self.locator['currency_selector'], locator_type="xpath")
+        # self.element_click(locator=new_list[5], locator_type="xpath")
+        # print(new_list, file=sys.stderr)
+        # currency_value = self.get_element_list(self.locator['currency_amount_option'], locator_type="xpath")
 
 bc = BrowserConfiguration("chrome")
 driver = bc.web_driver_instance()
